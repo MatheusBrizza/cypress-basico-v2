6a5363b5370e8ffab097a14b3f5a8f256b9c1234
Matheus <matheusbj95@gmail.com>
2023-06-06 19:21:41 -0300

    Exercício 1 feito

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,29 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Matheus')
+        cy.get('#lastName').type('Brizola')
+        cy.get('#email').type('matheus@teste.com')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+})
+
+
