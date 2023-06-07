569ec15904058556de3b585a697a669ff46d5a39
Matheus <matheusbj95@gmail.com>
2023-06-07 18:10:03 -0300

    Exercício extra 2 aula 3 concluído

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -89,8 +89,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     // Exercícios aula 3
 
-    it.only('seleciona um produto (YouTube) por seu texto', function() {
+    it('seleciona um produto (YouTube) por seu texto', function() {
         cy.get('#product').select('YouTube').should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+    })
+
+    it.only('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product').select(1).should('have.value', 'blog')
     })
 })
 
